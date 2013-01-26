ae20e4ffbb4a4dfbfa4aaa6e928b45a3746ba2dd
Steven Robbins <ste.robbins@gmail.com>
2013-01-26 18:18:06 +0000

    Working nuget build

--- rakefile.rb
+++ rakefile.rb
@@ -9,13 +9,20 @@ task :default do
 end
 
 namespace :razor do
+  ## Can't grab this from the assembly info because it uses ifdefs'
+  VERSION = "2.0"
+
   ASPNET_GIT_PATH = 'https://git01.codeplex.com/aspnetwebstack.git'
   WORKING_PATH = 'Working'
   RAZOR_DIR = 'System.Web.Razor'
   SOURCE_PATH = 'aspnetwebstack/src'
   RAZOR_PROJECT = "#{WORKING_PATH}/#{SOURCE_PATH}/#{RAZOR_DIR}/System.Web.Razor.csproj"
   BIN_LOCATION = "#{WORKING_PATH}/#{SOURCE_PATH}/bin/Release"
+  SHARED_ASSEMBLY_INFO = "#{WORKING_PATH}/#{SOURCE_PATH}/CommonAssemblyInfo.cs"
   CONFIGURATION = 'Release'
+  NUGET_EXE = 'tools/nuget.exe'
+  NUSPEC = 'System.Web.Razor.Unofficial.nuspec'
+  NUGET_OUTPUT_DIR = 'output'
 
   Dir.class_eval do
     def self.logged_chdir(dir, &block)
@@ -53,5 +60,37 @@ namespace :razor do
   end
 
   desc "Builds tne nuget"
+  task :build_nuget => :build do
+    puts "Version #{VERSION}"
+
+    update_xml NUSPEC do |xml|
+      xml.root.elements["metadata/version"].text = VERSION
+    end
+
+    FileUtils.rm_rf(NUGET_OUTPUT_DIR) if File.exists? NUGET_OUTPUT_DIR
+    Dir.mkdir(NUGET_OUTPUT_DIR)
+
+    nuget = NuGetPack.new
+    nuget.command = NUGET_EXE
+    nuget.nuspec = NUSPEC
+    nuget.output = NUGET_OUTPUT_DIR
+    nuget.execute
+  end
+
+  def update_xml(xml_path)
+    #Open up the xml file
+    xml_file = File.new(xml_path)
+    xml = REXML::Document.new xml_file
+
+    #Allow caller to make the changes
+    yield xml
+
+    xml_file.close
+
+    #Save the changes
+    xml_file = File.open(xml_path, "w")
+    formatter = REXML::Formatters::Default.new(5)
+    formatter.write(xml, xml_file)
+    xml_file.close
   end
 end
